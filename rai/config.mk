8eb13e14d28169de099ce998b4a94a310ed0040a
Tom Schouten <tom@zwizwa.be>
2013-09-02 16:39:39 -0400

    thinking about live coding

--- rai/config.mk
+++ rai/config.mk
@@ -3,15 +3,15 @@
 # The Steinberg VST SDK 2.4 can not be included in this distribution.
 # Get it at: http://www.steinberg.net/en/company/developer.html
 
-# VST_DIR := $(HOME)/kmook/vst/vstsdk2.4
+VST_DIR := $(HOME)/kmook/vst/vstsdk2.4
 # VST_DIR := $(HOME)/vstsdk2.4
-VST_DIR := C:/home/tom/vstsdk2.4
+# VST_DIR := C:/home/tom/vstsdk2.4
 
 
 # MinGW prefix for compiling Windows DLL/EXE
 
 ## Debian-hosted mingw32msvc
-# MINGW := i586-mingw32msvc-
+MINGW := i586-mingw32msvc-
 
 ## Cygwin-hosted mingw
 #MINGW := i686-pc-mingw32-
@@ -20,5 +20,5 @@ VST_DIR := C:/home/tom/vstsdk2.4
 # set PATH=C:\MinGW\bin;%PATH%
 # set PATH=C:\MinGW\msys\1.0\bin;%PATH%
 # set PATH=C:\Progra~1\Racket\;%PATH%
-MINGW := mingw32-
+# MINGW := mingw32-
 
