104312c1fc0a07c853b3e316be405533d0d6d05b
Tom Schouten <tom@zwizwa.be>
2013-09-22 10:54:14 -0400

    gdb sync on ^

--- rai/gdb.rkt
+++ rai/gdb.rkt
@@ -1,15 +1,23 @@
-#lang scheme/base
-(require scheme/file
-         scheme/system
-         scheme/pretty
+#lang racket/base
+(require racket/file
+         racket/system
+         racket/pretty
+         racket/async-channel
+         racket/match
+         racket/dict
          "gdb-parse.rkt")
 
 ;; Experimenting with GDB's MI
 
+;; TODO: everything is async right now.  How to implement synchronous
+;; commands on top of this?  What reply to look for? '^'?
+
+
 (provide (all-defined-out))
 
 (define gdb-instance (make-parameter #f))
-(define-struct gdb (stdout stdin pid stderr control thread-stdout thread-stderr) #:transparent)
+(define-struct gdb (stdout stdin pid stderr control
+                    thread-stdout thread-stderr channel) #:transparent)
 
 (define (close-gdb)
   (let ((g (gdb-instance)))
@@ -26,21 +34,22 @@
     (let* ((process-info (process cmdline))
            (stderr (list-ref process-info 3))
            (stdout (list-ref process-info 0))
-           (t-stderr (thread (lambda () (line-dispatch stderr stderr-handle))))
-           (t-stdout (thread (lambda () (line-dispatch stdout mi-handle)))))
+           (channel (make-async-channel))
+           (t-stderr (thread (lambda () (line-dispatch stderr stderr-handle channel))))
+           (t-stdout (thread (lambda () (line-dispatch stdout mi-handle channel)))))
       (gdb-instance
        (apply make-gdb
               (append process-info
-                      (list t-stderr t-stdout)))))))
+                      (list t-stderr t-stdout channel)))))))
 
-;; Dispatcher task body.  Abort
-(define (line-dispatch port handle)
+;; Dispatcher task body.  Abort on error or EOF.
+(define (line-dispatch port handle channel)
   (with-handlers
-      ((void void)) ;; just abort.  geiser doesn't like async printing..
+      ((void void))
     (let loop ()
       (let ((line (read-line port)))
         (unless (eof-object? line)
-          (handle line)
+          (handle line channel)
           (loop))))))
 
 ;; Geiser gets confused when async i/o hits stdout, so log things to file instead.
@@ -50,30 +59,41 @@
     #:exists 'append))
 
 ;; Does this actually get any data?
-(define (stderr-handle line)
+(define (stderr-handle line channel)
   (gdb-log "stderr: ~a\n" line))
 
 ;; Handle mi protocol
 ;; https://sourceware.org/gdb/onlinedocs/gdb/GDB_002fMI-Output-Syntax.html#GDB_002fMI-Output-Syntax
 
-(define (mi-log-data tag data)
+(define (mi-log-data tag-char data channel)
   (let* ((result (gdb-read-expr-list
                   (open-input-string data)))
-         (port (open-output-string)))
-    (pretty-print (cons (string->symbol (list->string (list tag)))
-                        result) port)
-    (gdb-log (get-output-string port))))
+         (port (open-output-string))
+         (tag (string->symbol (list->string (list tag-char))))
+         (msg (cons tag result)))
+    (begin
+      (pretty-print msg port)
+      (gdb-log (get-output-string port)))
+    (async-channel-put channel msg)))
 
 (define *data* #f)
-(define (mi-handle line)
+(define (mi-handle line channel)
   (let ((tag  (string-ref line 0))
         (data (substring  line 1)))
     (case tag
       ((#\() (void)) ;; "(gdb)" = ready for next command
       ((#\~ #\@ #\&) (gdb-log (read (open-input-string data))))
-      ((#\^ #\= #\* #\+) (mi-log-data tag data))
+      ((#\^ #\= #\* #\+) (mi-log-data tag data channel))
       (else (gdb-log "untagged: ~a\n" line)))))
 
+
+(define (autoconnect)
+  (unless (gdb-instance)
+    (start-gdb)
+    (connect)))
+
+(define (gdb-reply)
+   (async-channel-get (gdb-channel (gdb-instance))))
 ;; Execute GDB MI command
 (define (mi> cmd . args)
   (with-handlers
@@ -84,10 +104,27 @@
           (pretty-print ex)
           (close-gdb)
           (start-gdb))))
+    (autoconnect)
     (let ((p (gdb-stdin (gdb-instance))))
       (display (apply format cmd args) p)
       (newline p)
-      (flush-output p))))
+      (flush-output p)
+
+      ;; Wait for reply.  For each command, threre is at least one ^
+      ;; reply to use for synchronization.  Not 100% sure if this is
+      ;; correct, or what to do with the other tags..  Also it's not
+      ;; sure if it makes sense to have a task+channel indirection
+      ;; instead of just reading from gdb's stdout in the current
+      ;; thread.
+      (let loop ()
+        (let ((reply (gdb-reply)))
+          ;; (pretty-print reply)
+          (match reply
+            ((list-rest '^ msg)
+             (cdr reply))
+            (else
+             (loop)))))
+      )))
 
 ;; Execute regular GDB command
 (define (gdb> cmd)
@@ -96,7 +133,6 @@
 (define (disconnect)
   (mi> "-target-disconnect"))
 (define (connect [port 3333])
-  (start-gdb) ;; start if necessary
   (disconnect)
   (mi> "-gdb-set target-async 1")
   (mi> "-target-select remote :~s" port)
@@ -111,8 +147,11 @@
   (gdb> "set $pc = *(void**)4"))
 
 (define (dasm [n 5])
-  (mi> (format "-data-disassemble -s $pc -e \"$pc + ~s\" -- 0"
-               (* 2 n))))
+  (let ((result
+         (mi> (format "-data-disassemble -s $pc -e \"$pc + ~s\" -- 0"
+                      (* 2 n)))))
+    result))
+    
        
 
 
@@ -124,7 +163,7 @@
 ;; (define parse-seq (make-parameter #f))
 
 
-;; (require scheme/sequence)
+;; (require racket/sequence)
 ;; (define (port->stream p)   (sequence->stream (in-input-port-chars p)))
 ;; (define (string->stream s) (port->stream (open-input-string s)))
 
@@ -148,7 +187,7 @@
 ;;   (parameterize ((current-input-port (open-input-string str)))
 ;;     (fn)))
 
-;; (require scheme/pregexp)
+;; (require racket/pregexp)
     
 
 ;(define (test)
