a879626c716a7c7299045558618265912bd3f2f1
Tom Schouten <tom@zwizwa.be>
2014-01-25 16:04:43 +0100

    cleanup

---
+++ rai/ai-proc.rkt
@@ -0,0 +1,43 @@
+#lang racket/base
+(require "tools.rkt"
+         "ai-array-c.rkt"
+         "libproc.rkt"
+         racket/file
+         racket/system)
+
+(provide 
+         ai-proc
+         ai-proc-run-once
+         ;; ai-sp ;; FIXME: should this be exported?
+         )
+
+;; Convert stream function to C code, compile and run
+;; - Block parameters are not used: inputs are streams
+;; - Only run once - don't keep track of state: allows to abstract away instantiation
+
+(define (ai-sp proc)
+  (let* ((.g.h (make-temporary-file "proc-~a.g.h" #f "."))
+         (.sp  (regexp-replace ".g.h" (path->string .g.h) ".sp")))
+    (with-output-to-file .g.h
+      (lambda ()
+        (display (ai-array-c proc)))
+      #:exists 'truncate)
+    (let ((compile-out
+           (with-output-to-string
+             (lambda () (system (format "make ~a" .sp))))))
+      (delete-file .g.h)
+      .sp)))
+
+(define (ai-proc proc)
+  (let ((.sp (ai-sp proc)))
+    (let ((sp (proc_load_sp .sp)))
+      (delete-file .sp) 
+      sp)))
+
+;; Single run, don't keep state.
+(define (ai-proc-run-once proc-class ins/n [outs #f])
+  (let ((p (proc-instantiate proc-class)))
+    (proc-run p ins/n outs)))
+  
+
+
--- rai/test-ai-array-load.rkt
+++ rai/test-ai-array-load.rkt
@@ -1,48 +1,14 @@
 #lang racket
 
-(require "tools.rkt"
-         "ai-array.rkt"
-         "ai-array-c.rkt"
-         "stream-syntax.rkt"
-         "stream-lib.rkt"
-         "libproc.rkt"
+(require "ai-proc.rkt"
+         "stream-lib.rkt" ;; integrate
          ffi/vector)
 
-;; This is a simple form, not using block rate parameters.  All inputs
-;; are stream inputs.
-
-(define (ai-sp proc)
-  (let* ((.g.h (make-temporary-file "proc-~a.g.h" #f "."))
-         (.sp  (regexp-replace ".g.h" (path->string .g.h) ".sp")))
-    (with-output-to-file .g.h
-      (lambda ()
-        (display (ai-array-c proc)))
-      #:exists 'truncate)
-    (let ((compile-out
-           (with-output-to-string
-             (lambda () (system (format "make ~a" .sp))))))
-      (delete-file .g.h)
-      .sp)))
-
-
-(define (ai-proc proc)
-  (let ((.sp (ai-sp proc)))
-    (let ((sp (proc_load_sp .sp)))
-      ;; Since it's loaded, filesystem link can be removed already.
-      (delete-file .sp) 
-      sp)))
-
-
-
-;; Single run, don't keep state.
-(define (proc-once proc-class ins/n [outs #f])
-  (let ((p (proc-instantiate proc-class)))
-    (proc-run p ins/n outs)))
   
 (define sp-class (ai-proc integrate))
 
 (define ins  (list (list->f32vector (map exact->inexact '(1 1 1 1 1 1)))))
-(define outs (map f32vector->list (proc-once sp-class ins)))
+(define outs (map f32vector->list (ai-proc-run-once sp-class ins)))
 
 outs
 
