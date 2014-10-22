6961aec79ee0f6faf43c60def7b18fa4dfe818b6
Tom Schouten <tom@zwizwa.be>
2014-10-22 10:00:38 -0400

    impedance

--- test/test-impedance.rkt
+++ test/test-impedance.rkt
@@ -1,5 +1,6 @@
 #lang scheme/base
-(require scheme/pretty)
+(require scheme/pretty
+         scheme/dict)
 
 ;; Simple analog circuit simulator
 
@@ -20,25 +21,75 @@
 ;; Equations in normalized form: (equations eq eq ...) where eq = 0
 
 ;; To make this do anything, start with something simple: an RC filter.
-;; The basic building block is an n-port.  A complete circuit is a 0-port.
+
+;; The basic building block is an n-port.
+;; A complete circuit is a 0-port.
+
+;; Connecting (equating) nodes means all v are the same and the sum of
+;; currents is 0.
+
+;; All nodes are explicit. It is up to the user to not connect them.
+;; A non-connected node is a node connected to a 0 current source with
+;; a floating, non-specified voltage.
 
 (define p
   (nport
+   ;; n-port composition is terminal node connection:
+   ;; For each connected node, voltages are equal and currents sum to 0.
+   ;; Current is flowing into node from the
    (terminals
-    ;; Current flowing into node from the outside.
     (v_in  i_in)
     (v_out i_out)
     (v_gnd i_gnd))
+   ;; Intermediate variables used to factor equations.
+   (intermediates
+    (v_c))
+   ;; Externally specified parameters, i.e. matrix coefficients.
    (parameters
     (R C))
+   ;; System equations
    (equations
-    ;; n-ports do not have hidden supply nodes, so this always holds.
-    ;; Does it need to be explicit?
-    (+ i_gnd i_in i_out)
-    
-    (+ i_gnd (* C (d/dt (- v_out v_gnd))))
+    (- v_c (- v_out v_gnd))
+    (+ i_gnd (* C (d/dt v_c)))
     (- i_in  (* R (- v_in v_out)))
+    ;; n-ports do not have hidden supply nodes, so this current sum
+    ;; equation always holds for terminal nodes.  Does it need to be
+    ;; explicit?
+    (+ i_in i_out i_gnd)
     )))
  
    ;; Diode
    ;; (- (/ Id Is) (exp (/ Vx Vt)))
+
+;; mapaccum?
+
+(define (fold-subexpressions equation fn [s '()])
+  (if (not (pair? equation)) s
+      (for/fold
+          ((s (fn equation s)))
+          ((expr (cdr equation)))
+        (pretty-print expr)
+        (fold-subexpressions expr fn s))))
+
+(define (fold-equations-subexpressions eqs fn [s '()])
+  (for/fold
+      ((s s))
+      ((eq eqs))
+    
+    (fold-subexpressions eq fn s)))
+
+(define (nport-equations p) (dict-ref p 'equations))
+
+(define (collect fn)
+  (lambda (expr s)
+    (let ((v (fn expr)))
+      (if v (cons v s) s))))
+  
+(define (derivatives p)
+  (fold-equations-subexpressions
+   (nport-equations p)
+   (collect
+    (lambda (expr)
+      (and (pair? expr)
+           (eq? 'd/dt (car expr))
+           (cadr expr))))))
