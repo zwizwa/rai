169f6d0c28dbe10681e82f540c742243e7659d52
Tom Schouten <tom@zwizwa.be>
2014-12-01 22:00:26 -0500

    test_saw.rkt

---
+++ test/test_saw.rkt
@@ -0,0 +1,19 @@
+#lang s-exp rai/stream
+(require rai/stream-lib)
+(provide (all-defined-out))
+
+(define main-nsi 0)
+(define main-tc '())
+
+  
+
+(define (main samplerate)
+  (let*-values
+      (((isr)   (/ 1 samplerate))
+       ((t)     (* isr (nats)))
+       ((f_osc) (* t 1000 isr))
+       ((p)     (* .25 (phasor f_osc -.5 .5))))
+    (values p (+ 0.01 p))))
+
+
+  
