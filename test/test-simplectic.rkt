90ee35f3fa07cb5ab799361a7f4e817fe7f025e8
Tom Schouten <tom@zwizwa.be>
2014-12-16 12:08:53 -0500

    simplectic

---
+++ test/test-simplectic.rkt
@@ -0,0 +1,12 @@
+#lang scheme/base
+
+;; 1. Start with a simple hamiltonian: a harmonic oscillator.
+
+;; 2. Compute partial differentials using autodiff to yield differential equations.
+
+;; 3. Approximate using a symplex step
+
+;; 4. Compute invariant?
+
+
+
