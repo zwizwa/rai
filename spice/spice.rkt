ea00fe621914530ac53a5985a2c0bd40ed4f8607
Tom Schouten <tom@zwizwa.be>
2016-02-17 21:52:45 -0500

    spice

--- spice/spice.rkt
+++ spice/spice.rkt
@@ -27,21 +27,54 @@
   (for/list
       ((line (in-lines (open-input-file file)))
        ;; Skip comments
-       #:when (not (regexp-match #rx"^\\*" line))
+       #:when (not (regexp-match #rx"^[\\*+.]" line))
        ;; Ignore anything past .end
        #:break (regexp-match #rx"^\\.end" line)
        )
     (match (string-split line)
       ((cons name args)
        (let*-values
-           (((type)     (string->symbol (substring name 0 1)))
-            ((name)     (string->symbol (format "_~a" (substring name 1))))
-            ((n)        (nb-terminals type))
-            ((nets rst) (split-at args n)))
-         (list type
-               name
+           (((tag)      (string->symbol (substring name 0 1)))
+            ((inst)     (string->symbol (format "_~a" (substring name 1))))
+            ((nb)       (nb-terminals tag))
+            ((nets val) (split-at args nb)))
+         (list tag
+               inst
+               val
                (map string->number nets)
-               rst))))))
+               ))))))
+
+;; Build a graph representing the network.
+;; Every net corresponds to a node with 2 or more edges
+
+;; A Net has:
+;; - a signgle V,dV/dt
+;; - a I.dI/dt for each connected port (KCL: sum I = 0)
+;; - a port is represented by (instance, index)
+;; - an instance is (type name) iso (tag inst val)
+;; - type = (tag val)
+;; - name = (tag inst)
+
+(define sem-graph
+  `((T .,(lambda (g tiv n)
+           ;; Terminals are abstracted as voltage sources.
+           g))
+    (Q .,(lambda (g tiv c b e)
+           g))
+    (V .,(lambda (g tiv p m)
+           g))
+    (R .,(lambda (g tiv a b)
+           g))
+    ))
+
+
+;; An interesting analysis is to build a small-signal model.  Since
+;; that will be a circuit consisting only of 2-terminals, it is a
+;; proper non-simple graph and can be visualized with GraphViz.  A
+;; 2-terminal can then be represented as edge -> box -> edge.  Nodes
+;; can then be drawn smaller.
+
+
 
 (define (test file)
   (system (format "make ~s" file))
