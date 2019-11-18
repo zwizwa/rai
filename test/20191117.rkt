23b051d2807ad5b25bd653c157a7742b04ebf417
Tom Schouten <tom@zwizwa.be>
2019-11-17 21:53:43 -0500

    20191117

--- test/20191117.rkt
+++ test/20191117.rkt
@@ -10,13 +10,13 @@
 (define-values
   (main main-defaults)
   (lambda/params (samplerate)
-    (let* ((f     '0.0011)
-           (tick  (float (timer '5700.0)))    ;; 3500 - 6100 
-           (tick1 (float (timer '5200.0)))    ;; 3500 - 6100 
+    (let* ((f     '0.0017)
+           (tick  (float (timer '4700.0)))    ;; 3500 - 6100 
+           (tick1 (float (timer '4500.0)))    ;; 3500 - 6100 
            (e1    (env-AR tick '0.46 '0.0013)) ;; .01 - .0003  
            (e2    (env-AR tick1 '1.0 '0.0019)) ;; .00052 - .01
            (s     (* .1 (* e1 (saw-d3 f))))
-           (l     (svf-lp s (* e2 '120.0 f) '0.0092))
+           (l     (svf-lp s (* e2 '120.0 f) '0.069))
            )
       
       ;; (fdn4 x (vector 113 227 1397 11101.0))
@@ -24,7 +24,7 @@
       ;; (megasaw6)
       ;; (tinpan)
       
-      (* '0.069 l)
+      (* '0.91 l)
       
       )))
 
